43fe904fca83051c9368ac9fcaca01ffa4001206
JKyngS <domjoab@gmail.com>
2024-08-20 11:02:55 -0300

    foi salvo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("Por Favor, digite sua conta: ");
+
+        int numero = Integer.valueOf(args[0]);
+
+        System.out.println("Por Favor, digite o número da Agencia!");
+
+        String agencia = args[1];
+
+        System.out.println("Por Favor, digite seu nome: ");
+        
+
+        String nome = args[2];
+
+        System.out.println("Por Favor, digite seu saldo");
+
+        double saldo = Double.valueOf(args[3]);
+
+        
+        System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agencia é: " + agencia + " ,conta: " + numero + " e seu saldo: " + saldo + " já está disponível para saque"  );
+
+
     }
 }
+
+
