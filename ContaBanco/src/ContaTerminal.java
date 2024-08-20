d2c6a0ad420153208b61b9a483aa69e69645db15
JKyngS <domjoab@gmail.com>
2024-08-20 11:03:21 -0300

    Corrige leitura de nome completo no ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,24 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Por Favor, digite sua conta: ");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        int numero = Integer.valueOf(args[0]);
+        System.out.println("Por Favor, digite sua conta: ");
+        int numero = scanner.nextInt();
 
         System.out.println("Por Favor, digite o número da Agencia!");
+        String agencia = scanner.next();
 
-        String agencia = args[1];
-
+        scanner.nextLine();
         System.out.println("Por Favor, digite seu nome: ");
-        
-
-        String nome = args[2];
+        String nome = scanner.nextLine();
 
         System.out.println("Por Favor, digite seu saldo");
+        double saldo = scanner.nextDouble();
 
-        double saldo = Double.valueOf(args[3]);
-
-        
         System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, sua agencia é: " + agencia + " ,conta: " + numero + " e seu saldo: " + saldo + " já está disponível para saque"  );
 
 
